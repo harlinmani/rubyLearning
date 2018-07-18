65eaacd686a020aa2bca0540ac7472d70d4cb5f7
Harlin Mani <harlin.manifreshworks.com>
2018-07-18 17:01:35 +0530

    changes in dispatcher

--- railsProject/product_delivery_system/app/models/dispatcher.rb
+++ railsProject/product_delivery_system/app/models/dispatcher.rb
@@ -2,6 +2,6 @@ class Dispatcher < ActiveRecord::Base
   attr_accessible :address_id, :status, :dispatcher_id, :product_id, :quantity
   self.primary_key = "dispatcher_id"
 
-  belongs_to :product_id
-  belongs_to :address_id
+  belongs_to :products
+  belongs_to :addresses
 end
